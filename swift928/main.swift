07ed4b75140bf7d27b3a6fa1466390bf75302167
s20171103183 <s20171103183@admindeimac-35.local>
2018-09-28 20:11:40 +0800

    Initial Commit

---
+++ swift928/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift928
+//
+//  Created by s20171103183 on 2018/9/28.
+//  Copyright © 2018年 s20171103183. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
